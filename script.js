fff3bb84cccf270025dcae7907fab9f25f84779e
Amner Kalandarov <amner256@yahoo.com>
2019-06-03 16:51:25 +0000

    ok

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 /*global $*/
 $("#button").click(function(){
+
+$("#result").empty();
 var time = $("#year").val();
 var name= $("#title").val();
+var length= $("#type").val();
+
+
 
 $.ajax({
-url:"https://www.omdbapi.com/?t="+name+"&y=" + time + "&apikey=b976566f",
+url:"https://www.omdbapi.com/?t="+name+"&y=" + time + "plot="+ length +"&apikey=b976566f",
 
 method:"GET",
 success: function(response){
-console.log(response);
-
+$("#result").append("<p> Title:"+response.Title +"<br>"+ "Release Year:"+ response.Year+"</p>");
+$(".inputs").val();
 }
 });
+});
+$("#add").click(function(){
+   var addMovie=$("#result").text();
+   $("#list").append(addMovie+"<br>");
+   
+   
 });
